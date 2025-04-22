788b3d9a84f6ca6a1845cabcf37e4cad8ba2594b
pvillacorta <pablo.villacorta@uva.es>
2025-04-22 19:37:07 +0200

    Update GRE sequence parameters and add rotate_phantom function for improved phantom manipulation

--- mrm/Jemris_PC/main.jl
+++ mrm/Jemris_PC/main.jl
@@ -4,46 +4,23 @@ using KomaMRI, CUDA, StatsBase, JLD2, JSON3
 
 include("../sequences/GRE.jl")
 include("../utils/divide_spins_ranges.jl") 
+include("rotate_phantom.jl")
 
 ## ---- Phantom ----
-obj = read_phantom("../phantoms/fortin.phantom")
-obj.motion = NoMotion()
-obj = obj[abs.(obj.x) .<= 0.005]
-# sigma = 5e-4
-# obj.x .+= sigma .* randn(length(obj))
-# obj.x .+= sigma .* randn(length(obj))
-# obj.x .+= sigma .* randn(length(obj))
+obj = read_phantom("../phantoms/fortin_float32_Nt200.phantom")
+obj.T1 .= 850e-3
+obj.T2 .= 5e-3 
+obj = rotate_phantom(obj)
 ## ---- Scanner ---- 
 sys = Scanner(Gmax=80e-3, Smax=200, GR_Δt=1e-5, RF_Δt=1e-5, ADC_Δt=1e-5)
 
 ## ---- Sequence ----
-# Overload KomaMRI `*` function to avoid the following: ---------
-# Rotating an array of gradients (in x, y, and z) with 
-# different durations causes all durations to become the same
-# https://github.com/JuliaHealth/KomaMRI.jl/issues/545
-## Execute this only if the GitHub issue above is not solved:
-# import KomaMRIBase: *
-# function *(α::Matrix, x::Array{Grad})
-#     y = deepcopy(x)
-#     A_values = [g.A for g in x]  
-#     A_result = α * A_values     
-#     for (i, g) in enumerate(y)
-#         g.A = A_result[i]     
-#     end
-#     return y
-# end
-
-##
-# obj = brain_phantom2D()
-# obj.z .= -obj.x
-# obj.y .= obj.y
-# obj.x .= 0
-# ---------------------------------------------------------------
-
 fov      = [0.18, 0.13]
-N_matrix = [92, 66]
+# N_matrix = [92, 66]
 # N_matrix = [115, 83]
-# N_matrix = [461, 333]
+# N_matrix = [230, 166]
+N_matrix = [461, 333]
+
 res = fov ./ N_matrix
 
 TE = 10e-3
@@ -51,36 +28,43 @@ TR = 16e-3
 flip_angle = 15.0
 slice_thickness = 2.5e-3
 
-R = [ 0.  0. 1. ; 
-      0.  1. 0. ; 
-     -1.  0. 0. ]
-
-# R = [ 1.  0. 0. ; 
-#       0.  1. 0. ; 
-#       0.  0. 1. ]
-
 #VENC
-vs                  = "vx" 
+vs                  = "vz" 
 venc                = 200.0 .* 1e-3
 venc_durations_flat = 0.8   .* 1e-3
 venc_durations_rise = 0.1   .* 1e-3
 
 ##
-kspace      = []
-magnitude   = []
-phase       = [] 
-seqs        = Sequence[]
-acqs        = []
+# obj = brain_phantom2D()
+# obj.T2 .= TR/4 # Manual spoiling
 
-rg = 1:1
+##
+# L = 0.1
+# dx = 1e-3
 
-MAX_SPINS_PER_GPU = 200_000
-sequential_parts = divide_spins_ranges(length(obj), MAX_SPINS_PER_GPU)
+# r = -L/2:dx:L/2
+# x = repeat(r, outer=(1, length(r)))
+# y = repeat(r', outer=(length(r), 1))
+
+# obj = Phantom(x=x[:], y=y[:])
+# obj.T2 .= TR/4 # Manual spoiling
 
 ## ---- Sequence ----
-seq = read_seq("../sequences/PC_2D_tra_1dVz_92x66_TE10_TR16.seq")
-seq_a = R*seq[1:convert(Int, length(seq)/2 -1)]
-seq_b = R*seq[convert(Int, length(seq)/2)+1:end-1]
+seqs = Sequence[]
+# seq = read_seq("../sequences/PC_2D_tra_1dVz_$(N_matrix[1])x$(N_matrix[2])_TE10_TR16_flipangle15.seq")
+
+# # seq = read_seq("gre_jemris.seq")
+
+# # for i in 1:1
+# for i in findall(is_RF_on.(seq))
+#     # Make RF real
+#     seq[i].RF.A[1] .= abs.(seq[i].RF.A[1]) .* (-1).^(abs.(angle.(seq[i].RF.A[1])) .> 0.05)
+# end
+
+# seq_a = seq[1:convert(Int, length(seq)/2 -1)]
+# seq_b = seq[convert(Int, length(seq)/2)+1:end-1]
+
+# seq_a = seq_b = seq
 
 seq_a, seq_b, _ = PC_GRE(
     venc,
@@ -91,27 +75,31 @@ seq_a, seq_b, _ = PC_GRE(
     TR,
     flip_angle,
     sys;
-    R = R,
     pulse_duration = 1e-3,
-    adc_duration = 10e-3,
+    adc_duration = 2e-3,
     venc_duration_flat = venc_durations_flat,
     venc_duration_rise = venc_durations_rise,
-    balanced = true,
-    crusher_duration = 1e-3,
-    crusher_area = 4 * π / (2 * π * γ) / res[1],
+    balanced = false,
+    spoiled = false,
+    flow_compensation = true,
     slice_thickness = slice_thickness,
-    rf_phase_offset = 0
+    rf_phase_offset = π/2,
+    dummy_pulses = 0
 )
 
 push!(seqs, seq_a)
 push!(seqs, seq_b)
 
 ## ---- Simulation ----
-for seq in seqs
+MAX_SPINS_PER_GPU = 200_000
+raw_data    = []
+@time for seq in seqs
     sim_params = KomaMRICore.default_sim_params()
     sim_params["Nblocks"] = 5000
-    sim_params["Δt"]    = 8e-4
-    sim_params["Δt_rf"] = 1e-5
+    # sim_params["Δt"]    = 8e-4
+    # sim_params["Δt_rf"] = 1e-5
+
+    sequential_parts = divide_spins_ranges(length(obj), MAX_SPINS_PER_GPU)
 
     if length(sequential_parts) > 1
         @info "Dividing phantom ($(length(obj)) spins) into $(length(sequential_parts)) parts that will be simulated sequentially"
@@ -125,21 +113,29 @@ for seq in seqs
         push!(raws, simulate(obj[sequential_part], seq, sys))
     end
     raw = reduce(+, raws)
+    push!(raw_data, raw)
+end
 
-    ## ---- Reconstruction ----
+## ---- Reconstruction ----
+magnitude   = []
+phase       = [] 
+for r in raw_data
+    raw = deepcopy(r)
+    # Add gaussian noise
+    σ = 7
+    for i in 1:N_matrix[2]
+        noise = randn(ComplexF32, (N_matrix[1], 1)) .* σ
+        raw.profiles[i].data .+= noise
+    end
+    acqData = AcquisitionData(raw)
     recParams = Dict{Symbol,Any}(:reco=>"direct")
     Nx, Ny = N_matrix
     recParams[:reconSize] = (Nx, Ny)
     recParams[:densityWeighting] = false
-    push!(acqs, raw)
-    acqData = AcquisitionData(raw)
-    seq_no_rot = inv(R) * seq
-    _, ktraj = get_kspace(seq_no_rot)
     # Kdata
     acqData.kdata[1] = reshape(acqData.kdata[1],(Nx*Ny,1))
     # Traj
     acqData.traj[1].circular = false
-    acqData.traj[1].nodes = transpose(ktraj[:, 1:2])
     acqData.traj[1].nodes = acqData.traj[1].nodes[1:2,:] ./ maximum(2*abs.(acqData.traj[1].nodes[:]))
     acqData.traj[1].numProfiles = Ny
     # subsampleIndices
@@ -152,37 +148,34 @@ for seq in seqs
     push!(phase,     angle.(recon[:,:,1]))
 end
 
-
-## Plot results
 magnitude_mean = (magnitude[1] .+ magnitude[2]) ./ 2
 phase_diff     = -(mod.(phase[1] .- phase[2] .+ π, 2π) .- π)
 
-magnitudes = [magnitude[1], magnitude[2], magnitude_mean]
-phases     = [phase[1], phase[2], phase_diff]
+plot_image(magnitude_mean)
+plot_image(phase_diff; colorscale="Jet", height=300, width=600, title="Velocity map (mm/s)")
 
-magnitude_titles = ["Magnitude A", "Magnitude B", "Mean Magnitude (A+B)/2"] 
-phase_titles     = ["Phase A", "Phase B", "Phase A - Phase B"]
+## ---- Save ----
+save("result_$(N_matrix[1])x$(N_matrix[2])_Nt200.jld2", Dict("raw_data" => raw_data, "magnitude" => magnitude, "phase" => phase, "magnitude_mean" => magnitude_mean, "phase_diff" => phase_diff))
 
-fig = KomaMRIPlots.make_subplots(
-    rows=2, cols=3, 
-    subplot_titles= hcat(magnitude_titles, phase_titles),
-    shared_yaxes=true, 
-    shared_xaxes=true,
-    vertical_spacing=0.05,
-    horizontal_spacing=0.0
+## ---- Load ----
+data = load("result_$(N_matrix[1])x$(N_matrix[2])_Nt200.jld2")
+raw_data = data["raw_data"]
+magnitude = data["magnitude"]
+phase = data["phase"]
+magnitude_mean = data["magnitude_mean"]
+phase_diff = data["phase_diff"]
+
+## Test slice thickness
+rf1 = seq_a[1]
+z = range(-5., 5., 200) * 1e-3; # -5 to 5 mm
+M1 = simulate_slice_profile(rf1; z)
+
+using KomaMRIPlots.PlotlyJS
+plot(
+    scatter(x=z*1e3, y=abs.(M1.xy), name="Slice 1"),
+    Layout(xaxis=attr(title="z [mm]"), height=300,margin=attr(t=40,l=0,r=0), title="Slice profiles for the slice-selective sequence")
 )
 
-for i in 1:3
-    KomaMRIPlots.add_trace!(fig, plot_image(magnitudes[i], zmin=minimum(vcat(magnitudes...)), zmax=percentile(vec(vcat(magnitudes...)), 99)).plot.data[1], row=1, col=i)
-    KomaMRIPlots.add_trace!(fig, plot_image(phases[i], zmin=-π, zmax=π, colorscale="Jet").plot.data[1], row=2, col=i)
-end
-
-display(fig)
-
-# savefig(fig, "PC_GRE.png"; height=800, width=1500)
-
-##
-data = [acqs[1].profiles[i].data for i in 1:66]
-kspace = hcat(data...)
-plot_image(abs.(kspace))
-
+## Get velocity
+V = venc .* phase_diff ./ π
+p = plot_image(V .* 1e3; colorscale="Jet", height=300, width=600, title="Velocity map (mm/s)")
--- mrm/sequences/GRE.jl
+++ mrm/sequences/GRE.jl
@@ -79,32 +79,23 @@ function PC_GRE(
 	N::Union{Array,Real},
 	TE::Real, TR::Real, 
 	α, sys::Scanner;
-	Gss=[0, 0, 4.5e-3], 
-	R = collect(I(3)),
 	pulse_duration = 3e-3,
-	adc_duration = 0.0,
+	adc_duration = 3e-3,
 	venc_duration_flat = 0.1e-3,
 	venc_duration_rise = 1.8e-3,
 	balanced = false,
+	spoiled = false,
+	flow_compensation = false,
 	crusher_duration = 1e-3,
 	crusher_area = 0.0,
 	slice_thickness = 2e-3,
 	z0 = 0.0,
 	rf_phase_offset = 0.0,
+	dummy_pulses = 0
 )
 	# If only fov and N are provided in one dimension: Square acquisition
 	FOV = FOV isa Array ? FOV : [FOV,FOV]
 	N = N isa Array ? N : [N,N]
-
-    # Excitation (Sinc pulse) ----------------------------------
-
-	# B_1° = 2.59947e-7 / (pulse_duration * 1e3)
-	# B1 = α*  B_1°
-	# EX = PulseDesigner.RF_sinc(-1im*B1,pulse_duration,sys;G=Gss,Δf=Δf)
-
-	# EX[2].GR[3].A = 3*EX[2].GR[3].A
-	# EX[2].GR[3].T = EX[2].GR[2].T = EX[2].GR[1].T = EX[2].GR[3].T/3
-	# EX.DUR[2] = EX[2].GR[3].T + 2*EX[2].GR[3].rise
 
 	# SINC pulse parameters 
 	Trf = pulse_duration
@@ -132,15 +123,12 @@ function PC_GRE(
 			return B1
 		end
 	end
+
 	EX = PulseDesigner.RF_sinc(get_b1(B1, rf_phase_offset), Trf, sys; G=[0; 0; Gz], TBP=TBP, Δf=f0)
 
-	# EX[2].GR[3].A = 3*EX[2].GR[3].A
-	# EX[2].GR[3].T = EX[2].GR[2].T = EX[2].GR[1].T = EX[2].GR[3].T/3
-	# EX.DUR[2] = EX[2].GR[3].T + 2*EX[2].GR[3].rise
-
 	# Acquisition ----------------------------------------------
-	ζ_phase = 0.05e-3
-	T_phase = 0.15e-3
+	ζ_phase = 0.2e-3
+	T_phase = 0.6e-3
 
 	Δkx, Δky = 1 ./ FOV
 	FOVkx, FOVky = (N .- 1) .* [Δkx, Δky]
@@ -153,20 +141,25 @@ function PC_GRE(
 	# Bipolar gradients
 	bipolar = bipolar_gradients(venc,  direction, venc_duration_flat, venc_duration_rise) 
 
+	# Flow compensation
+	flow_compensated_ss = Sequence()
+	flow_compensated_fe = Sequence()
+
+	if flow_compensation
+		# Slice selection
+		EX[2].GR[3].A = 2*EX[2].GR[3].A
+		flow_compensated_ss = -EX[2]/2
+		# Frecuency encoding
+		grad = Grad(1, T_phase, ζ_phase)
+		flow_compensated_fe = Sequence([(Gx/2)*grad; 0*grad;  0*grad;;])
+		Gx = 2*Gx
+	end
+
 	# FE and Readout
-	if adc_duration == 0.0
-		TE_min = sys.ADC_Δt*(N[1]-1)/2 + EX.DUR[1]/2 + EX.DUR[2] + dur(bipolar) + T_phase + 2*ζ_phase
-		if TE < TE_min
-			print("TE too short. Setting it to TE_min = ", TE_min*1e3, " ms\n")
-			TE = TE_min
-		end
-		adc_duration = 2 * (TE - ( EX.DUR[1]/2 + EX.DUR[2] + dur(bipolar) + T_phase + 2*ζ_phase ))
-	else
-		TE_min = max(sys.ADC_Δt*(N[1]-1), adc_duration)/2 + EX.DUR[1]/2 + EX.DUR[2] + dur(bipolar) + T_phase + 2*ζ_phase
-		if TE < TE_min
-			print("TE too short. Setting it to TE_min = ", TE_min*1e3, " ms\n")
-			TE = TE_min
-		end
+	adc_duration_min = sys.ADC_Δt*(N[1]-1)
+	if adc_duration < adc_duration_min
+		print("adc_duration too short. Setting it to adc_duration_min = ", adc_duration_min*1e3, " ms.")
+		adc_duration = adc_duration_min
 	end
 
 	G_ro = FOVkx/(γ*adc_duration)
@@ -176,32 +169,46 @@ function PC_GRE(
 	RO = Sequence(GR)
 	RO.ADC[1] = ADC(N[1], T_ro, ζ_ro)
 
+	TE_min = dur(RO)/2 + EX.DUR[1]/2 + EX.DUR[2] + dur(flow_compensated_ss) +  dur(bipolar) + 2*T_phase + 4*ζ_phase
+	if TE < TE_min
+		print("TE too short. Setting it to TE_min = ", TE_min*1e3, " ms\n")
+		TE = TE_min
+	end
+	delay_TE = TE - TE_min
+
 	t_balanced = balanced ? T_phase + 2*ζ_phase : 0.0
-	TR_min = dur(EX) + dur(bipolar) + T_phase + 2*ζ_phase + dur(RO) + max(t_balanced, crusher_duration)
+	crusher_duration = spoiled ? crusher_duration : 0.0
+	TR_min = dur(EX) + dur(flow_compensated_ss) + delay_TE + dur(bipolar) + 2*T_phase + 4*ζ_phase + dur(RO) + max(t_balanced, crusher_duration)
 	if TR < TR_min
 		print("TR too short. Setting it to TR_min = ", TR_min*1e3, " ms\n")
 		TR = TR_min
 	end
 	delay_TR = TR - TR_min
 
-	crusher = Grad(crusher_area/(0.9*crusher_duration), 0.8*crusher_duration, 0.1*crusher_duration) 
+	crusher = spoiled ? Grad(crusher_area/(0.9*crusher_duration), 0.8*crusher_duration, 0.1*crusher_duration) : Grad(0., 0., 0.)
 
 	gre_a = Sequence()
 	gre_b = Sequence()
 	gre   = Sequence()
 
-	for i in 0:(N[2]-1)
-		gre_a += R * EX + bipolar
-		gre_b += R * EX - bipolar
-		gre   += R * EX + Delay(dur(bipolar))
+	for i in 0:(dummy_pulses + N[2] - 1)
+		gre_a += EX + flow_compensated_ss + Delay(delay_TE) 
+		gre_b += EX + flow_compensated_ss + Delay(delay_TE) 
+		gre   += EX + flow_compensated_ss + Delay(delay_TE) 
 
 		grad = Grad(1, T_phase, ζ_phase)
-		dephase          =            Sequence([(-Gx/2)*grad; (-Gy/2 + i*step)*grad;  0*grad;;])
-		balanced_dephase = balanced ? Sequence([0*crusher; 	  (Gy/2 - i*step)*grad;  0*crusher;;]) : Sequence()
+		dephase          =            Sequence([(-Gx/2)*grad; (Gy/2 - i*step)*grad;  0*grad;;])
+		balanced_dephase = balanced ? Sequence([0*crusher; 	  (-Gy/2 + i*step)*grad;   crusher;;]) : Sequence()
 
-		gre_a += R * (dephase + RO + balanced_dephase + Delay(delay_TR))
-		gre_b += R * (dephase + RO + balanced_dephase + Delay(delay_TR))
-		gre   += R * (dephase + RO + balanced_dephase + Delay(delay_TR))
+		if i in 0:dummy_pulses-1
+			gre_a += Delay(dur(bipolar + flow_compensated_fe + dephase + RO + balanced_dephase) + delay_TR)
+			gre_b += Delay(dur(bipolar + flow_compensated_fe + dephase + RO + balanced_dephase) + delay_TR)
+			gre   += Delay(dur(bipolar + flow_compensated_fe + dephase + RO + balanced_dephase) + delay_TR)
+		else
+			gre_a += ( bipolar            + flow_compensated_fe + dephase + RO + balanced_dephase + Delay(delay_TR))
+			gre_b += (-bipolar            + flow_compensated_fe + dephase + RO + balanced_dephase + Delay(delay_TR))
+			gre   += (Delay(dur(bipolar)) + flow_compensated_fe + dephase + RO + balanced_dephase + Delay(delay_TR))
+		end
 	end
 
 	gre_a.DEF = Dict("Nx"=>N[1],"Ny"=>N[2],"Nz"=>1,"Name"=>"gre_a_"*string(N[1])*"x"*string(N[2]),"FOV"=>[FOV[1], FOV[2], 0])
