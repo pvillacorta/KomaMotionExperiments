5105c5034aae635764cebc9f8c06b4d9be3eed48
Pablo Villacorta Aylagas <pablo.villacorta@uva.es>
2025-06-04 11:14:16 +0200

    Add README and update generate_jemris_phantom.jl for static and dynamic phantom files

---
+++ mrm/2.3b_JEMRIS_PC/fortin_phantom/README.md
@@ -0,0 +1,19 @@
+# KomaMRI Phantom Conversion Files
+
+This directory contains the files used to generate a KomaMRI-compatible version of the flow phantom described in [Fortin et al. (2021)](https://doi.org/10.1002/mrm.27114).
+
+> **Note**: These files are **not required** to reproduce the experiments by Fortin et al., as the KomaMRI-compatible phantom is already available for direct download from [Zenodo](https://zenodo.org/records/15591102).
+
+## Purpose
+
+The scripts and files in this directory are provided for transparency and reproducibility regarding the conversion process. They allow users to see how the original phantom data (in JEMRIS format) was transformed into a KomaMRI-compatible format.
+
+## Overview of Files
+
+- **`.dat` files**: These are spin trajectory files downloaded automatically from [Zenodo](https://zenodo.org/records/15591102). They are formatted for use with JEMRIS.
+- **`generate_jemris_phantom.jl`**: A Julia script that generates the `.h5` file representing the JEMRIS phantom.
+- **`jemris_flow_phantom_to_koma.jl`**: A Julia script that converts the spin trajectories and JEMRIS phantom into a format compatible with KomaMRI.
+
+## Summary
+
+While this directory is not essential for using the KomaMRI phantom directly, it serves as documentation of the conversion process from the original data provided by Fortin et al. to the KomaMRI format.
--- mrm/2.3b_JEMRIS_PC/fortin_phantom/generate_jemris_phantom.jl
+++ mrm/2.3b_JEMRIS_PC/fortin_phantom/generate_jemris_phantom.jl
@@ -2,7 +2,8 @@ cd(@__DIR__)
 
 using HDF5
 
-fid = h5open("sample.h5", "w")
+# Static phantom file for JEMRIS
+fid = h5open("sample_static.h5", "w")
 
 sample = create_group(fid, "sample")
 
@@ -12,13 +13,33 @@ T2  = 5f-3
 T2s = 0f0
 Δw  = 0f0
 
-sample["data"]       = permutedims(  [PD  .* ones(Float32, (2069943, 1));;;
-                                      T1  .* ones(Float32, (2069943, 1));;;
-                                      T2  .* ones(Float32, (2069943, 1));;;
-                                      T2s .* ones(Float32, (2069943, 1));;;
-                                      Δw  .* ones(Float32, (2069943, 1))], (3,2,1)  )
+sample["data"]       = permutedims(  [PD  .* ones(Float32, (419109, 1));;;
+                                      T1  .* ones(Float32, (419109, 1));;;
+                                      T2  .* ones(Float32, (419109, 1));;;
+                                      T2s .* ones(Float32, (419109, 1));;;
+                                      Δw  .* ones(Float32, (419109, 1))], (3,2,1)  )
 sample["offset"]     = zeros(Float32, (1, 3))
 sample["resolution"] = zeros(Float32, (1, 3))
 
 close(fid)
 
+# Dynamic phantom file for JEMRIS
+fid = h5open("sample_dynamic.h5", "w")
+
+sample = create_group(fid, "sample")
+
+PD  = 1f0
+T1  = 850f-3
+T2  = 5f-3 
+T2s = 0f0
+Δw  = 0f0
+
+sample["data"]       = permutedims(  [PD  .* ones(Float32, (1650834, 1));;;
+                                      T1  .* ones(Float32, (1650834, 1));;;
+                                      T2  .* ones(Float32, (1650834, 1));;;
+                                      T2s .* ones(Float32, (1650834, 1));;;
+                                      Δw  .* ones(Float32, (1650834, 1))], (3,2,1)  )
+sample["offset"]     = zeros(Float32, (1, 3))
+sample["resolution"] = zeros(Float32, (1, 3))
+
+close(fid)
